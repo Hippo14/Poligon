8927a46931f470b1f8ad439f9215e87151d09757
Hippo14 <hippo14@gmail.com>
2015-12-20 13:39:33 +0100

    Hello World!

---
+++ src/main/SMO.java
@@ -0,0 +1,7 @@
+package main;
+
+/**
+ * Created by MSI on 2015-12-20.
+ */
+public class SMO {
+}
---
+++ src/main/narzedzia/Wykresy.java
@@ -0,0 +1,7 @@
+package main.narzedzia;
+
+/**
+ * Created by MSI on 2015-12-20.
+ */
+public class Wykresy {
+}
